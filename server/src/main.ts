4103754ec0032ddfe583fddf3733c6f21d4aac38
david <fivelanes72@gmail.com>
2025-09-30 01:44:51 +0100

    feat: Initial commit for micro-commerce-app including server

---
+++ server/src/main.ts
@@ -0,0 +1,44 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['log', 'fatal', 'error', 'warn', 'debug', 'verbose'],
+  });
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Micro-Commerce-App API')
+    .setDescription(
+      'Developer documentation for Micro-Commerce-App API store platform',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port);
+
+  console.log(
+    `🚀 micro-commerce is running on: http://localhost:${process.env.PORT ?? 4001}`,
+  );
+  console.log(
+    `📘 Swagger Docs available at: http://localhost:${port}/api/docs`,
+  );
+}
+void bootstrap();
